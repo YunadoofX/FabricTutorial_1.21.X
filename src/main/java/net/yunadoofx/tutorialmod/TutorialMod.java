208e3d70e74e4c2fb00441fe927761b86199b0a0
Yuv C <aadityakrishna426@gmail.com>
2025-01-03 11:51:27 -0600

    added super amethyst block

--- src/main/java/net/yunadoofx/tutorialmod/TutorialMod.java
+++ src/main/java/net/yunadoofx/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.yunadoofx.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.yunadoofx.tutorialmod.block.ModBlocks;
 import net.yunadoofx.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 
 	}
 }
